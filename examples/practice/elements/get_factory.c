92c7ac44ae66fea1fd67457a4c5da3c01d7c3a56
Imjae Park <ijai.whiz@gmail.com>
2018-11-13 23:42:16 +0900

    get_factory.c: Add another generating element factory source
    I also comment some functions description.
    signed-off-by:Imjae Park

--- examples/practice/elements/get_factory.c
+++ examples/practice/elements/get_factory.c
@@ -2,28 +2,34 @@
 
 int main(int argc, char* argv[])
 {
-    GstElementFactory *factory;
-    GstElement *element;
+	GstElementFactory *factory;
+	GstElement *element;
 
-    /* init Gstreamer */
-    gst_init(&argc, &argv);
+	/* init GStreamer */
+	gst_init(&argc, &argv);
 
-    /* create element, method #2 */
-    factory = gst_element_factory_find("fakesrc");
-    if(!factory)
-    {
-        g_print("Failed to find factory of type 'facesrc`\n");
-        return -1;
-    }
+	/* create element, method#2 */
+	/* Search for an element factory of the given name.
+	   Refs the returned element factory; caller is responsible for unreffing.
+	   Returns GstElementFactory if found, NULL otherwise */
+	factory = gst_element_factory_find("fakesrc");
+	if(!factory)
+	{
+		g_print("Failed to find factory of type 'fakesrc'\n");
+		return -1;
+	}
 
-    element = gst_element_factory_create(factory, "source");
-    if(!element)
-    {
-        g_print("Failed to create element, even though its factory exist!\n");
-        return -1;
-    }
+	/* is it difference between gst_element_factory_create and gst_element_factory_make? */
+	/* gst_element_factory_create() creates an element using element factory.
+	   gst_element_factory_make() create an element using factory name. */
+	element = gst_element_factory_create(factory, "source");
+	if(!element)
+	{
+		g_print("Failed to create element, even though its factory exists!\n");
+		return -1;
+	}
 
-    gst_object_unref(GST_OBJECT(element));
+	gst_object_unref(GST_OBJECT(element));
 
     return 0;
 }
