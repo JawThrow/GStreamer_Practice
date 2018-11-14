c3ea088dad71dbe852ae432abbe98bf61a81a20d
Imjae Park <ijai.whiz@gmail.com>
2018-11-14 23:14:10 +0900

    get_element_name.c: Modify getting element name function
    I replaced g_object_get() with gst_object_get_name().
    New function uses GstObject type parameter whereas g_object_get() used gpointer.
    Also, i added gst_object_set_name() to name element

--- examples/practice/elements/get_element_name.c
+++ examples/practice/elements/get_element_name.c
@@ -10,9 +10,10 @@ int main(int argc, char* argv[])
 
     /* create element */
     element = gst_element_factory_make("fakesrc", "source");
-
+	gst_object_set_name(GST_OBJECT(element), "SRC");
     /* get name */
-    g_object_get(G_OBJECT(element), "name", &name, NULL);
+	//g_object_get(G_OBJECT(element), "name", &name, NULL);
+	name = gst_object_get_name(GST_OBJECT(element)); // don't be confused in G_OBJECT() and GST_OBJECT()
     g_print("the name of the element is '%s'.\n", name);
     g_free(name);
 
