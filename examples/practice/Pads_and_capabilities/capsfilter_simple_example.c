9e7d96b9356f153bdd5f51724bf2bff5054ee2d2
Imjae Park <ijai.whiz@gmail.com>
2018-11-22 20:42:36 +0900

    Correct mistakes in these sources.
    Add input parameter(gst_struct_new)) when gst_caps_new_full() is called,
    Fix type(gst_element_link_filter -> gst_element_link_filtered).

--- examples/practice/Pads_and_capabilities/capsfilter_simple_example.c
+++ examples/practice/Pads_and_capabilities/capsfilter_simple_example.c
@@ -8,7 +8,7 @@ static gboolean link_elements_with_filter(GstElement* element1, GstElement *elem
 				   "height", G_TYPE_INT, 288,
 				   "framerate", GST_TYPE_FRACTION, 25, 1, NULL);
 
-	link_ok = gst_element_link_filtered(element1, element2, caps); // insert capsfilter inside element1 and element2?
+	link_ok = gst_element_link_filtered(element1, element2, caps); // insert customed capsfilter element between element1 and element2?
 	gst_caps_unref(caps);
 
 	if(!link_ok)
